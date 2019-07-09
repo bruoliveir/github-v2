10a0ffb4de2c123353c17a18b4396cd839ee2cf4
Bruno Oliveira <bru.oliveir@gmail.com>
2019-07-08 22:32:19 -0300

    Configures buildSrc

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,20 @@
+object ApplicationId {
+    val id = "bru.oliveir.playground"
+}
+
+object Versions {
+    val compileSdk = 29
+    val minSdk = 21
+    val targetSdk = 29
+    val gradle = "3.4.1"
+    val kotlin = "1.3.41"
+}
+
+object Releases {
+    val versionCode = 1
+    val versionName = "1.0"
+}
+
+object KotlinLibraries {
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+}
