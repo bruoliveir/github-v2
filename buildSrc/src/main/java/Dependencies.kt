0dbc50625703d323d2c7b1c0bef064c4fb62c637
Bruno Oliveira <bru.oliveir@gmail.com>
2019-07-09 11:44:13 -0300

    Adds structure for a new feature

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,6 +2,10 @@ object ApplicationId {
     val id = "bru.oliveir.playground"
 }
 
+object Modules {
+    val featureMaster = ":features:master"
+}
+
 object Versions {
     val compileSdk = 29
     val minSdk = 21
@@ -9,6 +13,11 @@ object Versions {
     val gradle = "3.4.1"
     val kotlin = "1.3.41"
     val koin = "2.0.1"
+    val appCompat = "1.0.2"
+    val junit = "4.12"
+    val androidTestRunner = "1.2.0"
+    val espresso = "3.2.0"
+    val glide = "4.9.0"
 }
 
 object Releases {
@@ -22,4 +31,15 @@ object KotlinLibraries {
 
 object Libraries {
     val koin = "org.koin:koin-android:${Versions.koin}"
+    val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+}
+
+object AndroidLibraries {
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+}
+
+object TestLibraries {
+    val junit = "junit:junit:${Versions.junit}"
+    val androidTestRunner = "androidx.test:runner:${Versions.androidTestRunner}"
+    val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
 }
---
+++ features/master/src/androidTest/java/bru/oliveir/master/ExampleInstrumentedTest.java
@@ -0,0 +1,25 @@
+package bru.oliveir.master;
+
+import android.content.Context;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("bru.oliveir.master.test", appContext.getPackageName());
+    }
+}
